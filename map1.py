ce117abe362fed0b9469d677d5013e6bb9dd880d
francescobongini <bongini.francesco@gmail.com>
2021-01-15 15:24:06 +0100

    Upd

--- map1.py
+++ map1.py
@@ -85,12 +85,12 @@ def valid(datacfg, cfgfile, weightfile, outfile):
 
 if __name__ == '__main__':
     import sys
-    log = open("myprog.log", "a")
+    log = open("myprog_box_rand.log", "a")
     sys.stdout = log
-    for i in [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]:
+    for i in list(range(48,51)):
         datacfg = 'data/flir.data'
         cfgfile = 'cfg/yolov3_flir.cfg'
-        weightfile = 'backup/bbox/yolov3_flir_{:06d}.weights'.format(i)#weights/yolov3_flir.weights
+        weightfile = 'backup/randaug_bbox/yolov3_flir_{:06d}.weights'.format(i)#weights/yolov3_flir.weights
         outfile = 'det_test_'
 
         if len(sys.argv) == 2:
